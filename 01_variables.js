555c955fb728dd8d6ad60bb1f43004bd0f7dc6af
TalhaTahir24 <165506239+TalhaTahir24@users.noreply.github.com>
2024-07-13 20:58:23 +0500

    Create 01_variables.js

---
+++ 01_variables.js
@@ -0,0 +1,18 @@
+const Developer= "Talha"
+let producer = "Tayyeb"
+var client="Suleman"
+age = 19
+
+/*
+These are variables for storing some data in memory.
+Prefer not to use 'var' because value is changed every time.
+and always use some variable for storing data.
+*/
+
+console.table([Developer,producer,producer,client,age,client,])
+
+
+// const Developer=("abc")   do not do, this throws an error, because const value dose not change.
+
+producer = "abc"
+client="abc"
